a271e5312dc416b3759b67a316d70e0748a514f4
유재건 <neversaydie374@gmail.com>
2023-11-01 21:47:24 +0900

    release test

--- cooking/src/main/java/com/cooking/first/entity/BoardDTO.java
+++ cooking/src/main/java/com/cooking/first/entity/BoardDTO.java
@@ -8,28 +8,28 @@ import lombok.ToString;
 @Getter
 @Setter
 public class BoardDTO {
-	 // 게시글 번호
-    private Double boardNum;
+	// 게시글 번호
+	private int boardNum = 0;
 
-    // 카테고리 도메인
-    private String catDomain;
+	// 카테고리 도메인
+	private String catDomain;
 
-    // 작성자
-    private String creator;
+	// 작성자
+	private String creator;
 
-    // 제목
-    private String titil;
+	// 제목
+	private String titil;
 
-    // 내용
-    private String content;
+	// 내용
+	private String content;
 
-    // 조회수
-    private int hit;
+	// 조회수
+	private int hit;
 
-    // 댓글 수
-    private int rplCnt;
+	// 댓글 수
+	private int rplCnt;
 
-    // 수정 이름
-    private String upName;
+	// 수정 이름
+	private String upName;
 
 }
